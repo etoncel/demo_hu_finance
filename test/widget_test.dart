131dd1953c4ba2b099603e7e6cfb51eb78c7d259
Juan Carlos Suarez Marin <juan.suarez@pragma.com.co>
2024-04-17 11:51:15 -0500

    refactor de getit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hu_basica/main.dart';
+import 'package:demo_hu_finance/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
