24d4027da90ff4af4c607cfa8dc89bc716dd539b
yulianhincapie <yulian.hincapie@pragma.com.co>
2024-04-17 16:33:50 -0500

    add test to repository

--- test/data/default_bank_services_repository_test.dart
+++ test/data/default_bank_services_repository_test.dart
@@ -1,35 +1,51 @@
-import 'package:demo_hu_finance/data/default_bank_services_repository.dart';
+import '../utils/get_it_util.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'default_bank_services_repository_test.mocks.dart';
 import 'package:demo_hu_finance/data/dto/bank_service_dto.dart';
 import 'package:demo_hu_finance/data/interface/api_client.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-import 'package:get_it/get_it.dart';
+import 'package:demo_hu_finance/data/default_bank_services_repository.dart';
+import 'package:demo_hu_finance/domain/repositories/bank_service_repository.dart';
 
-class MockApiClient extends Mock implements ApiClient {}
-
+@GenerateMocks([ApiClient])
 void main() {
-  GetIt getIt = GetIt.instance;
+  late ApiClient apiClient;
+  late BanKServiceRepository repository;
 
-  setUp(() {
-    getIt.registerFactory<ApiClient>(() => MockApiClient());
+  setUpAll(() {
+    apiClient = MockApiClient();
+    getItTest.registerFactory<ApiClient>(() => apiClient);
+    repository = DefaultBankServicesRepository();
   });
 
-  tearDown(() {
-    getIt.reset();
+  tearDownAll(() {
+    if (getItTest.isRegistered<ApiClient>()) {
+      getItTest.unregister<ApiClient>();
+    }
   });
 
-  test('should return a list of BankService when the call is successful',
-      () async {
-    final mockApiClient = getIt<ApiClient>();
-    final repository = DefaultBankServicesRepository();
-    final List<BankServiceDTO> mockServices = [
-      BankServiceDTO(name: 'Servicio itau 1'),
-    ];
+  test('get bankServices is empty', () async {
+    final List<BankServiceDTO> services = [];
 
-    when(mockApiClient.getServices())
-        .thenAnswer((_) async => (mockServices, null));
+    when(apiClient.getServices())
+        .thenAnswer((realInvocation) async => Future.value((services, null)));
 
     final result = await repository.getServices();
-    //expect(result.$2, isNull);
+
+    expect(result.$1.isEmpty, true);
+    verify(apiClient.getServices());
+  });
+
+  test('get bankServices is not empty', () async {
+    final List<BankServiceDTO> services = [BankServiceDTO(name: 'pse')];
+
+    when(apiClient.getServices())
+        .thenAnswer((realInvocation) async => Future.value((services, null)));
+
+    final result = await repository.getServices();
+
+    expect(result.$1.isEmpty, false);
+    verify(apiClient.getServices());
   });
 }
--- test/utils/get_it_util.dart
+++ test/utils/get_it_util.dart
@@ -1,13 +1,3 @@
 import 'package:get_it/get_it.dart';
 
-GetIt getIt = GetIt.instance;
-
-class GetItUtil {
-  static void registerDependency<T>() {
-    getIt.registerFactory(() => T);
-  }
-
-  static void unRegisterDependency<T>() {
-    getIt.unregister();
-  }
-}
+GetIt getItTest = GetIt.instance;
