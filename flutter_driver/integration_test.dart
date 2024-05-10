0762ba82641e7dba6061e4208f30957d45ad68d9
etoncel <ernesto.toncel@gmail.com>
2024-05-10 10:39:24 -0500

    add integration driver

---
+++ flutter_driver/integration_test.dart
@@ -0,0 +1,4 @@
+import 'package:integration_test/integration_test_driver.dart';
+
+// Se realiza integracion con el chromedriver para correr tests de integracion en el navegador chrome
+Future<void> main() => integrationDriver();
